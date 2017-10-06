02436475fe91d76a7ec09d74f4fff7025b594221
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2017-10-06 08:10:56 -0500

    UnitTesting_AdditionTest_MyCalculator

---
+++ app/src/test/java/com/mycalculator/juansandoval/mycalculator/CalculatorUnitTest.java
@@ -0,0 +1,14 @@
+package com.mycalculator.juansandoval.mycalculator;
+
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class CalculatorUnitTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
